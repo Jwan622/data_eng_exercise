e15e897f173aa3e938709d90d827ad5fd6bb2bf5
Jeffrey <Jwan622@gmail.com>
2020-10-25 21:48:11 -0400

    adding docker file and install script

---
+++ docker/postgres/CreateTables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE ad_events (
+    id serial PRIMARY KEY,
+    event_id varchar(255) NOT NULL,
+    phone_id varchar(255),
+    ad_id int,
+    provider varchar(255),
+    placement varchar(255),
+    length int,
+    event_ts date
+);
+
+CREATE TABLE user_events (
+    id serial PRIMARY KEY,
+    event_id varchar(255) NOT NULL,
+    user_id varchar(255),
+    property varchar(255),
+    value varchar(80),
+    event_ts date
+);
