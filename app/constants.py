2ddb2ec0848291b3c98be45406808ba9c581d2d0
Jeffrey <Jwan622@gmail.com>
2020-10-26 22:03:15 -0400

    add constant to get table columns for bulk insert

--- app/constants.py
+++ app/constants.py
@@ -4,7 +4,11 @@ USER_RECORDS_KEY = "user_records"
 AD_RECORDS_KEY = "ad_records"
 AD_TABLE_NAME = "ad_events"
 USER_TABLE_NAME = "user_events"
-EVENTS_TO_TABLE = {
+RECORD_KEY_TO_TABLE_NAME = {
     USER_RECORDS_KEY: USER_TABLE_NAME,
     AD_RECORDS_KEY: AD_TABLE_NAME
+}
+RECORD_KEY_TO_COLUMNS = {
+    AD_RECORDS_KEY: '(event_id, phone_id, ad_id, provider, placement, length, event_ts)',
+    USER_RECORDS_KEY: '(event_id, user_id, phone_id, property, value, event_ts)'
 }
